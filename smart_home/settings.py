89d6528d74fc00d6a46ed9c4827e7ff785de6c1c
ho0lah0op <129693028+ho0lah0op@users.noreply.github.com>
2024-01-31 21:12:29 +0400

    Update settings.py with HOST

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9m2%u2yq=+#)6c#^j7^1p+@mpiob-r)9ki+zrv%*ad779fz89l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['80.78.242.168']
+ALLOWED_HOSTS = ['89.111.169.9']
 
 
 # Application definition
